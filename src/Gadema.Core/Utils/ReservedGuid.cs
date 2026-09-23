282367a19a13b82742986fd59284753012085ca4
Baenoox <oliveremich@gmail.com>
2026-09-23 22:03:21 +0200

    added systemanchor to reserveguid to prevent guid.empty

--- src/Gadema.Core/Utils/ReservedGuid.cs
+++ src/Gadema.Core/Utils/ReservedGuid.cs
@@ -2,12 +2,15 @@ namespace Gadema.Core.Utils;
 
 public static class ReservedGuid
 {
+    private const byte SystemAnchor = 0x01;
     // The bit-shifting logic remains the same
     public static Guid Create(int moduleIndex, int serial)
     {
         byte[] bytes = new byte[16];
+        
         bytes[8] = (byte)((moduleIndex >> 8) & 0xFF);
         bytes[9] = (byte)(moduleIndex & 0xFF);
+        bytes[10] = SystemAnchor;
         bytes[12] = (byte)((serial >> 24) & 0xFF);
         bytes[13] = (byte)((serial >> 16) & 0xFF);
         bytes[14] = (byte)((serial >> 8) & 0xFF);
